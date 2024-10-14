c21d190414308f81980a8ee5178b72a996e0c344
d3sc <vinary686@gmail.com>
2024-10-14 18:34:49 +0700

    add features cashflow: delete data

---
+++ cashflow-daily.php
@@ -0,0 +1,229 @@
+<?php
+
+include "config/connection.php";
+
+$tanggalSaatIni = date("d");
+$bulanSaatIni = date('m');
+$tahunSaatIni = date('Y');
+$sql = "SELECT * FROM cashflow WHERE DAY(time) = $tanggalSaatIni AND MONTH(time) = $bulanSaatIni AND YEAR(time) = $tahunSaatIni";
+$result = mysqli_query($conn, $sql);
+
+$totalSpend = 0;
+while ($data = mysqli_fetch_array($result)) {
+    $angkaBersih = str_replace(['Rp.', ','], ['', ''], $data["spend"]);
+
+    // Mengganti koma desimal dengan titik (jika ada koma)
+    $angkaBersih = str_replace('.', '', substr($angkaBersih, 0, -3)) . '.' . substr($angkaBersih, -2);
+
+    // Mengonversi menjadi float
+    $angka = (float) $angkaBersih;
+
+    $totalSpend += $angka;
+}
+$totalSpend = "Rp." . number_format($totalSpend, 2, '.', ',');
+
+if (isset($_POST['delete'])) {
+    $id = $_POST['id'];
+
+    $sql = "DELETE FROM cashflow WHERE id=$id";
+    $result = mysqli_query($conn, $sql);
+    if ($result) {
+        header("location: cashflow-daily.php?success=record has been successfully deleted!");
+    }
+}
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.84.0">
+    <title>Dashboard Template · Bootstrap v5.0</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/dashboard/">
+
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- css datatables -->
+
+    <!-- dataTables -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.bootstrap5.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.datatables.net/2.1.6/js/dataTables.js"></script>
+    <script src="https://cdn.datatables.net/2.1.6/js/dataTables.bootstrap5.js"></script>
+
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
+
+
+    <!-- Custom styles for this template -->
+    <link href="css/dashboard.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Company name</a>
+        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+        <div class="navbar-nav">
+            <div class="nav-item text-nowrap">
+                <a class="nav-link px-3" href="#">Sign out</a>
+            </div>
+        </div>
+    </header>
+
+    <div class="container-fluid">
+        <div class="row">
+            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+                <div class="position-sticky pt-3">
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="#">
+                                <span data-feather="home"></span>
+                                Dashboard
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">
+                                <span data-feather="file"></span>
+                                Orders
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <span data-feather="shopping-cart"></span>
+                                Products
+                            </a>
+                        </li>
+                    </ul>
+
+                    <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                        <span>Saved reports</span>
+                        <a class="link-secondary" href="#" aria-label="Add a new report">
+                            <span data-feather="plus-circle"></span>
+                        </a>
+                    </h6>
+                    <ul class="nav flex-column mb-2">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <span data-feather="file-text"></span>
+                                Current month
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                    <h1 class="h2">Daily Cashflow</h1>
+                    <div class="btn-toolbar mb-2 mb-md-0">
+                        <div class="btn-group me-2">
+                            <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
+                            <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
+                        </div>
+                        <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
+                            <span data-feather="calendar"></span>
+                            This week
+                        </button>
+                    </div>
+                </div>
+
+                <a href="cashflow-daily/create.php" class="btn btn-primary my-3">
+                    Add Cashflow
+                </a>
+
+                <div class="d-flex justify-content-between align-item-center">
+                    <h2>History Daily Cashflow</h2>
+                    <h2>Total: <?php echo $totalSpend ?></h2>
+                </div>
+                <table id="example" class="table table-striped" style="width:100%">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Time</th>
+                            <th>Description</th>
+                            <th>Spend</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $index = 1;
+
+                        $result = mysqli_query($conn, $sql);
+                        while ($data = mysqli_fetch_array($result)):
+                        ?>
+                            <tr>
+                                <td><?php echo $index ?></td>
+                                <td><?php echo $data['time'] ?></td>
+                                <td><?php echo $data['description'] ?></td>
+                                <td><?php echo $data['spend'] ?></td>
+                                <td class="d-flex justify-content-center align-item-center gap-3">
+                                    <button class="btn btn-warning">Edit</button>
+                                    <form action="" method="POST">
+                                        <input type="text" name="id" value="<?php echo $data['id'] ?>" hidden>
+                                        <button type="submit" name="delete" class="btn btn-danger">Delete</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        <?php
+                            $index++;
+                        endwhile;
+                        ?>
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <th>No</th>
+                            <th>Time</th>
+                            <th>Description</th>
+                            <th>Spend</th>
+                            <th>Action</th>
+                        </tr>
+                    </tfoot>
+                </table>
+
+            </main>
+        </div>
+    </div>
+
+
+    <script src="assets/dist/js/bootstrap.bundle.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script>
+    <script src="js/dashboard.js"></script>
+    <script>
+        $(document).ready(function() {
+            $("#example").DataTable()
+        })
+    </script>
+</body>
+
+</html>
---
+++ cashflow-daily/create.php
@@ -0,0 +1,129 @@
+<?php
+require "../config/connection.php";
+
+// include "../helpers/path.php";
+// include "../middleware/isGuest.php";
+
+$error = "";
+
+if (isset($_POST["submit"])) {
+    $description = $_POST["description"];
+    $spend = $_POST["spend"];
+
+    if (empty($description) || empty($spend)) {
+        $error = "All input fields must be filled!";
+    }
+
+    if (!is_numeric($spend)) {
+        $error = "Investment amount must be a valid number!";
+    }
+
+    if (empty($error)) {
+        $spend = "Rp." . number_format($spend, 2, '.', ',');
+        $sql = "INSERT INTO cashflow (description, spend) VALUES ('$description', '$spend')";
+        $result = mysqli_query($conn, $sql);
+
+        if ($result) {
+            header("Location: ../index.php?success=record has been successfully added!");
+        } else {
+            $error = "Error: " . mysqli_error($conn);
+        }
+    }
+}
+
+?>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.84.0">
+    <title>Dashboard Template · Bootstrap v5.0</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/dashboard/">
+
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
+
+    <!-- Custom styles for this template -->
+    <link href="../css/dashboard.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <!-- header layouts -->
+    <?php include "./layouts/header.php" ?>
+
+    <div class="container-fluid">
+        <div class="row">
+
+            <!-- sidebar layouts -->
+            <?php include "./layouts/sidebar.php" ?>
+
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                    <h1 class="h2">Create Cashflow</h1>
+                </div>
+
+                <?php
+                if ($error) {
+                ?>
+                    <div class="alert alert-danger" role="alert">
+                        <?php echo $error ?>
+                    </div>
+                <?php
+                }
+                ?>
+
+                <div class="container">
+                    <div class="mt-4">
+                        <div class="container">
+                            <div class="mt-4">
+                                <!-- Form sederhana untuk create -->
+                                <form action="" method="post">
+                                    <div class="mb-3">
+                                        <label for="description" class="form-label">Description</label>
+                                        <input type="text" class="form-control" id="description" name="description" required>
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="spend" class="form-label">Spend</label>
+                                        <input type="number" class="form-control" id="spend" name="spend" required>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </div>
+        </main>
+    </div>
+    </div>
+
+</body>
+<script src="assets/dist/js/bootstrap.bundle.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script>
+<script src="js/dashboard.js"></script>
+
+</html>
